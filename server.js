08796ab8061a3cefe0f4bc064a6196c31c1a47e9
Azad <azad.63.mamedov@gmail.com>
2022-01-21 02:37:42 +0400

    fix static path

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const wsServer = new WebSocket.Server({ server });
 if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static(path.resolve(__dirname, 'front', 'build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'));
     });
 }
 
